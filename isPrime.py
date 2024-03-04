5b4bd00e3dfbee196baeaa7779329e456420e73f
Adam <brian.adam@sfgov.org>
2024-03-04 12:42:19 -0800

    Updated isPrime with new logic that has improved efficiency based on explanation of an algorithm for finding prime numbers. Broke up tests into separate functions.

--- isPrime.py
+++ isPrime.py
@@ -1,23 +1,23 @@
 #This is the week 2 assignment for OAF PSD
 #This code checks if a number is prime
+from math import isqrt
 
 def isPrime(number):
     #this code will not identify if a negative number is prime
-    if number < 0:
+    if number <= 3:
+        return number > 1
+    #if the number is even or divisible by 3
+    if (number % 2 == 0) or (number % 3 == 0):
         return False
-    #if the number is one of these, it's prime
-    elif number in [0, 1, 2, 3]:
-        return True
-    #if the number is even, it can't be prime
-    elif (number % 2 == 0):
-        return False
-    #check if anything divides the number
-    else:
-        i = 2
-        while i < number:
-            if number % i == 0:
-                return False
-            else:
-                i += 1
-        #if the while loop expires, return True
-        return True
+    #use square root as a limit for checking potential divisors
+    limit = isqrt(number)
+
+    #skip numbers that are divisible by 2 and 3
+    for i in range(5, limit+1, 6):
+        #check if current number & i+2
+        #this logic ensures untested numbers are not skipped
+        if number % i == 0 or number % (i+2) == 0:
+            return False
+    
+    #if the while loop expires, return True
+    return True
--- test_isPrime.py
+++ test_isPrime.py
@@ -1,9 +1,27 @@
 #test cases for isPrime, which returns True if the number is Prime
-from .isPrime import isPrime
+from isPrime import isPrime
 
-def test_isPrime():
+#these 3 tests confirm the first if statement works
+def test_isPrime_negative():
     assert isPrime(-2) == False
-    assert isPrime(0) == True
+
+def test_isPrime_zero():
+    assert isPrime(0) == False
+
+def test_isPrime_smallnumbers():
+    assert isPrime(2) == True
+
+#these two tests redundantly check the 2nd if statement
+def test_isPrime_evennum():
     assert isPrime(4) == False
+
+def test_isPrime_nonprimeoddnum():
     assert isPrime(15) == False
+
+#these tests confirm the functionality of the for loop
+#that generates divisors to test primality
+def test_isPrime_primenum():
     assert isPrime(17) == True
+
+def test_isPrime_largenumber():
+    assert isPrime(22409) == True
